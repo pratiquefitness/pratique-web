f0aeeef5529078544a800284226868b267c58a37
Henrique Zolini <henriquezolini@gmail.com>
2023-06-06 12:01:49 -0300

    alterar cor para o nutri

--- src/configs/theme.js
+++ src/configs/theme.js
@@ -3,8 +3,8 @@ import { theme as antheme } from 'antd'
 const theme = {
   algorithm: antheme.darkAlgorithm,
   token: {
-    colorPrimary: '#ed143d', //PRIME
-    //colorPrimary: '#acc571', //NUTRI
+    //colorPrimary: '#ed143d', //PRIME
+    colorPrimary: '#acc571', //NUTRI
     //colorPrimary: '#A2722A', //PRIME
     controlHeight: 50
   }
