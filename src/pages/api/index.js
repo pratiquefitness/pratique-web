6c7aedd2f306f69764b677037a96c6a7939fa9c5
Henrique Zolini <henriquezolini@gmail.com>
2023-07-11 16:19:14 -0300

    corrige prettier

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,7 +1,7 @@
-import prisma from '@/services/prisma'
+import { apiPratiqueAulas } from '@/services'
 
 export default async function handler(req, res) {
-  const data = await prisma.aulas.findMany()
+  const data = await apiPratiqueAulas.aulas.findMany()
   //const json = JSON.stringify(data, (key, value) => (typeof value === 'bigint' ? value.toString() : value))
   //res.status(200).json(JSON.parse(json))
   res.status(200).json(data)
