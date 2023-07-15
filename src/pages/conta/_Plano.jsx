f20d25e32341a8a3f140a72486ec79f122998b59
Henrique Zolini <henriquezolini@gmail.com>
2023-07-15 17:56:24 -0300

    adiciona tela de plano

--- src/pages/conta/_Plano.jsx
+++ src/pages/conta/_Plano.jsx
@@ -1,3 +1,16 @@
+import { Card, Space } from 'antd'
+import { useSelector } from 'react-redux'
+
 export default function Plano() {
-  return <div>Plano</div>
+  const { usuario } = useSelector(state => state.login)
+  return (
+    <Space direction="vertical" className="w-100">
+      <Card title="Seu plano" size="small">
+        <p>{usuario.plano}</p>
+      </Card>
+      <Card title="Sua unidade" size="small">
+        <p>{usuario.plano}</p>
+      </Card>
+    </Space>
+  )
 }
