bf816f8a919961043102a59b902a721e7469e33b
Henrique Zolini <henriquezolini@gmail.com>
2023-07-15 11:09:32 -0300

    corrige aulas coletivas

--- src/pages/api/aulas_coletivas.js
+++ src/pages/api/aulas_coletivas.js
@@ -2,14 +2,10 @@ import { apiPratiqueAulas } from '@/services'
 
 export default async function handler(req, res) {
   const { aula_categoria } = req.body
-  const data = await apiPratiqueAulas.aulas.findMany(
-    aula_categoria
-      ? {
-          where: {
-            aula_categoria: String(aula_categoria)
-          }
-        }
-      : {}
-  )
+  const data = await apiPratiqueAulas.aulas.findMany({
+    where: {
+      aula_categoria: String(aula_categoria)
+    }
+  })
   res.status(200).json(data)
 }
