68ebb0acefa569e87b344f79f29348c48664a164
Henrique Zolini <henriquezolini@gmail.com>
2023-07-20 08:05:27 -0300

    adiciona viewport de no zoom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,15 +3,21 @@ import store from '@/redux/store'
 import { Provider } from 'react-redux'
 import { AuthProvider } from '@/contexts/AuthContext'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AuthProvider>
-    </Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <Provider store={store}>
+        <AuthProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AuthProvider>
+      </Provider>
+    </>
   )
 }
