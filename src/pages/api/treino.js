abff674457cbb6474835127046479c0b333e9017
Henrique Zolini <henriquezolini@gmail.com>
2023-07-28 16:14:18 -0300

    remove cache do treino

--- src/pages/api/treino.js
+++ src/pages/api/treino.js
@@ -72,6 +72,6 @@ export default async function handler(req, res) {
       }
     }
   }
-
+  res.setHeader('Cache-Control', 'no-store')
   res.status(200).json(utils.clearDatabaseResult(usuarioExist))
 }
