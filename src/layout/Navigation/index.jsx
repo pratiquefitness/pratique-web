a4d3cc6ecc402e00e3ad8495ce4305051fcd5a7f
Henrique Zolini <henriquezolini@gmail.com>
2023-07-29 11:51:44 -0300

    corrige login do aluno acessando afiliados

--- src/layout/Navigation/index.jsx
+++ src/layout/Navigation/index.jsx
@@ -16,6 +16,8 @@ export default function Navigation({ data }) {
 
   const isHomeUser = usuario.plano?.includes('CASA') || false
 
+  const isAffiliate = !!usuario.isAffiliate
+
   useEffect(() => {
     setSelected(utils.getFirstLevelRoute(pathname))
   }, [pathname])
@@ -25,14 +27,24 @@ export default function Navigation({ data }) {
     router.push(item.href)
   }
 
+  const getWidth = () => {
+    const widthPerItem = 70
+    let width = 210
+    if (isHomeUser) width = width + widthPerItem
+    if (isAffiliate) width = width + widthPerItem
+    return width
+  }
+
   return (
-    <div className="navigation" style={{ background: token.colorPrimary, width: isHomeUser ? 350 : 280 }}>
+    <div className="navigation" style={{ background: token.colorPrimary, width: getWidth() }}>
       <ul>
         {data.map((item, key) => {
           const checkBike = item.href === '/bike' ? isHomeUser : true
+          const checkAfiliate = item.href === '/afiliados' ? isAffiliate : true
           return (
             item.showInNavigation &&
-            checkBike && (
+            checkBike &&
+            checkAfiliate && (
               <li className={item.href === selected ? 'list active' : 'list'} onClick={onNavigate(item)} key={key}>
                 <a style={{ pointerEvents: 'none' }}>
                   <span className="icon" style={{ color: item.href === selected ? token.colorPrimary : 'white' }}>
