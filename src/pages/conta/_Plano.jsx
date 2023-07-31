56a3bcc5628b57f6cf2db0c9877e6d1174ac9f76
Henrique Zolini <henriquezolini@gmail.com>
2023-07-31 17:33:21 -0300

    adiciona botao sac no plano

--- src/pages/conta/_Plano.jsx
+++ src/pages/conta/_Plano.jsx
@@ -1,4 +1,5 @@
-import { Card, Space } from 'antd'
+import { Button, Card, Col, Row, Space } from 'antd'
+import { FaPhone } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 
 export default function Plano() {
@@ -11,6 +12,14 @@ export default function Plano() {
       <Card title="Sua unidade" size="small">
         <p>{usuario.unidade}</p>
       </Card>
+      <a
+        href="https://api.whatsapp.com/send?phone=553141411962&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20quero%20falar%20sobre%20meu%20plano."
+        target="_blank"
+      >
+        <Button icon={<FaPhone />} block>
+          Falar com o SAC
+        </Button>
+      </a>
     </Space>
   )
 }
