458bfb2182ab5b2d2e2a2ed011b072db4061d08a
Henrique Zolini <henriquezolini@gmail.com>
2023-09-22 08:45:25 -0300

    ajusta banner para mostrar somente as proximas 5 lives

--- src/pages/api/lives.js
+++ src/pages/api/lives.js
@@ -4,11 +4,13 @@ import { format } from 'date-fns'
 export default async function handler(req, res) {
   const data = await apiPratiqueAulas.lives.findMany({
     where: {
-      live_datagravacao: format(new Date(), 'yyyy-MM-dd')
+      live_datagravacao: {
+        gte: format(new Date(), 'yyyy-MM-dd')
+      }
     },
     orderBy: {
       live_horagravacao: 'asc'
     }
   })
-  res.status(200).json(data)
+  res.status(200).json(data.slice(0, 5))
 }
