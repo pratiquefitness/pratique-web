6efcb2e711d5c6c2959aa73a4ed9c7af56e491a3
Henrique Zolini <henriquezolini@gmail.com>
2023-10-16 19:01:31 -0300

    corrige funcao de copy to clipboard

--- src/utils/index.js
+++ src/utils/index.js
@@ -66,10 +66,24 @@ const utils = {
     list = list.filter(value => searchText(value))
     return list
   },
+
   copyTextToClipboard: text => {
     if (!navigator.clipboard) {
-      fallbackCopyTextToClipboard(text)
-      return
+      const textarea = document.createElement('textarea')
+      textarea.value = text
+      textarea.setAttribute('readonly', '')
+      textarea.style.position = 'absolute'
+      textarea.style.left = '-9999px'
+      document.body.appendChild(textarea)
+      const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
+      textarea.select()
+      document.execCommand('copy')
+      document.body.removeChild(textarea)
+      if (selected) {
+        document.getSelection().removeAllRanges()
+        document.getSelection().addRange(selected)
+      }
+      return true
     }
     navigator.clipboard.writeText(text).then(
       function () {
