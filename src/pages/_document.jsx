6176c1c6b0589474bae4deaccdd121ee72105c06
Henrique Zolini <henriquezolini@gmail.com>
2023-11-08 15:39:11 -0300

    adiciona tag nosscript

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,6 +15,14 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P9HTD4HD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
