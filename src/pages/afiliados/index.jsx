0c4b58d74795e09ac478147b2ae8e76143bdc77e
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2023-12-09 17:09:07 -0300

    Update index.jsx

--- src/pages/afiliados/index.jsx
+++ src/pages/afiliados/index.jsx
@@ -3,6 +3,7 @@ import Geral from './_Geral'
 import Comissao from './_Comissao'
 import Produtos from './_Produtos'
 import PlanosAcademia from './_PlanosAcademia'
+import JumperFit from './_JumperFit'
 
 const items = [
   {
@@ -24,6 +25,11 @@ const items = [
     key: 'planos',
     label: `Planos Academia`,
     children: <PlanosAcademia />
+  },
+  {
+    key: 'jumperfit',
+    label: `Jumper Fit`,
+    children: <JumperFit />
   }
 ]
 
