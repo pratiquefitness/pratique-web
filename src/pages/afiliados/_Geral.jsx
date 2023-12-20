0dbd7e10897c8054865ac234558ca69bbb68c831
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2023-12-20 09:16:15 -0300

    Update _Geral.jsx

--- src/pages/afiliados/_Geral.jsx
+++ src/pages/afiliados/_Geral.jsx
@@ -155,19 +155,19 @@ export default function Geral() {
               <br />
               <p style={{ marginTop: '10px', textAlign: 'center' }}>Precisas de apoio?</p>
               <Col xs={24} className="mb-12">
-                <Button
-                  icon={<FaWhatsapp fill="#fff" size={30} />}
-                  style={{ background: 'green', color: 'white' }}
-                  block
-                  onClick={() => {
-                    window.open(
-                      'https://api.whatsapp.com/send?phone=5531984861766&text=Ol%C3%A1%2C%20sou%20afiliado%20Pratique%20e%20gostaria%20de%20ajuda',
-                      '_blank'
-                    )
-                  }}
+                <a
+                  href="https://api.whatsapp.com/send?phone=5531984861766&text=Olá,%20sou%20afiliado%20Pratique%20e%20gostaria%20de%20ajuda"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ display: 'block', textDecoration: 'none' }}
                 >
-                  SUPORTE AFILIADO
-                </Button>
+                  <Button
+                    icon={<FaWhatsapp fill="#fff" size={30} />}
+                    style={{ background: 'green', color: 'white', width: '100%' }}
+                  >
+                    SUPORTE AFILIADO
+                  </Button>
+                </a>
               </Col>
             </Col>{' '}
             <br /> <br /> <br /> <br />
