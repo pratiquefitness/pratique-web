091cde987aee43fff652ba5f993277265aadbe54
renatochristian <renatochristian@outlook.com>
2024-01-11 17:21:24 -0300

    barra do blog ajustada

--- src/components/Browser/index.jsx
+++ src/components/Browser/index.jsx
@@ -10,35 +10,33 @@ const { Title } = Typography
 const Browser = ({ url, title, onClose }) => {
   return url ? (
     <div className="browser-container">
-      <Header />
-
-      <div className="d-flex py-2 justify-space-between">
-        <Breadcrumb
-          separator={<DoubleRightOutlined className="" />}
-          className="ml-2 text-capitalize d-flex items-center"
-          items={[
-            {
-              title: <HomeOutlined />,
-              onClick: onClose
-            },
-            url === 'https://pratiquefitness.com.br/blog/' && {
-              style: 'line-height: 1.7;',
-              path: 'https://pratiquefitness.com.br/blog/',
-              title: 'Blog da Pratique'
-            },
-            url === 'https://www.clubecertosaude.com.br/saude/saversaude/' && {
-              style: 'line-height: 1.7;',
-              path: 'https://www.clubecertosaude.com.br/saude/saversaude/',
-              title: 'Saver Saúde'
-            },
-            url === '' && {
-              style: 'line-height: 1.7;',
-              title: `${url.substring(21)}`
-            }
-          ]}
-        />
-      </div>
-
+      {url !== 'https://pratiquefitness.com.br/blog/' && (
+        <>
+          <Header />
+          <div className="d-flex py-2 justify-space-between">
+            <Breadcrumb
+              separator={<DoubleRightOutlined className="" />}
+              className="ml-2 text-capitalize d-flex items-center"
+              items={[
+                {
+                  title: <HomeOutlined />,
+                  onClick: onClose
+                },
+                url === 'https://www.clubecertosaude.com.br/saude/saversaude/'
+                  ? {
+                      style: 'line-height: 1.7;',
+                      path: 'https://www.clubecertosaude.com.br/saude/saversaude/',
+                      title: 'Saver Saúde'
+                    }
+                  : url === '' && {
+                      style: 'line-height: 1.7;',
+                      title: `${url.substring(21)}`
+                    }
+              ]}
+            />
+          </div>
+        </>
+      )}
       <iframe className="browser-iframe" src={url} />
       <Affix offsetBottom={12}>
         <Navigation data={routes} />
