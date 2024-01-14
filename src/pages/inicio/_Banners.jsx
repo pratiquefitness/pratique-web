380c835f11e80b31cb759a060358117fe31d9aa7
renatochristian <renatochristian@outlook.com>
2024-01-14 20:23:26 -0300

    correção no banner inicial

--- src/pages/inicio/_Banners.jsx
+++ src/pages/inicio/_Banners.jsx
@@ -1,99 +1,108 @@
 import Loading from '@/components/Loading'
 import { getLives } from '@/redux/actions/lives'
-import utils from '@/utils'
-import { Carousel, Typography, theme } from 'antd'
-import { format } from 'date-fns'
+import { theme } from 'antd'
 import { useEffect } from 'react'
-import Countdown from 'react-countdown'
 import { useDispatch, useSelector } from 'react-redux'
 
-const contentStyle = {
-  margin: 5,
-  height: 200,
-  color: 'black',
-  borderRadius: 10
-}
-
-const settings = {
-  className: 'center',
-  centerMode: false,
-  slidesToShow: 1,
-  swipeToSlide: true,
-  draggable: true,
-  autoplay: true,
-  dots: true,
-  autoplaySpeed: 3000,
-  style: {
-    width: '100%'
-  }
-}
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import Link from 'next/link'
+import CarouselItem from './_CarouselItem'
 
 /*
 5 min - em instantes
 agora - acontecendo
 */
 
+const listaCarousel = [
+	{
+	  href: '/bike',
+	  image: '/images/power.png',
+	  isRounded: true
+	},
+	{
+	  href: '/aulas_coletivas/abdominais',
+	  image: '/images/banner_home/abdominais.png',
+	  isRounded: true
+	},
+	{
+	  href: '/aulas_coletivas/queimeacasa"',
+	  image: '/images/banner_home/queime_casa.png',
+	  isRounded: true
+	},
+]
+
+
 export default function Banners() {
-  const disptach = useDispatch()
+  const dispatch = useDispatch()
   const { token } = theme.useToken()
   const { data, loading } = useSelector(state => state.lives)
 
   useEffect(() => {
-    disptach(getLives())
+    dispatch(getLives())
   }, [])
 
   return (
     <Loading spinning={loading}>
       {data.length ? (
-        <Carousel {...settings}>
-          {data.map((live, key) => {
-            const datetime = new Date(`${live.live_datagravacao}T${live.live_horagravacao}:00`)
-            if (new Date() >= datetime) {
-              return false
+        <Carousel
+          additionalTransfrom={0}
+          arrows={false}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 3,
+              height: 300,
+              slidesToSlide: 1,
+              partialVisible: 'false'
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0
+              },
+              height: 400,
+              items: 1
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464
+              },
+              items: 2
             }
-            const dia = format(new Date(live.live_datagravacao + 'T00:00:00'), 'dd')
-            const mes = format(new Date(live.live_datagravacao + 'T00:00:00'), 'MM')
-            return (
-              <div key={key}>
-                <a href={`https://www.youtube.com/watch?v=${live.live_videoyoutube}`} target="_blank">
-                  <div
-                    style={{
-                      backgroundImage: `url('${live.live_link}')`,
-                      backgroundSize: 'cover',
-                      backgroundSize: 'contain',
-                      backgroundRepeat: 'no-repeat',
-                      backgroundPosition: 'right',
-                      backgroundColor: 'white',
-                      ...contentStyle
-                    }}
-                  >
-                    <div className="p-4">
-                      <h3>
-                        {`${dia} de ${utils.getMonthNames(mes).name} `}
-                        <small>às {live.live_horagravacao}H</small>
-                      </h3>
-                      <h2 style={{ color: token.colorPrimary }}>{live.live_nome}</h2>
-                      <p>
-                        <Countdown
-                          date={datetime}
-                          renderer={({ hours, minutes, seconds, completed }) => {
-                            return (
-                              <>
-                                Começa em: <br />
-                                <span style={{ fontWeight: 'bold', fontSize: 20 }}>
-                                  {hours}h {minutes}m {seconds}s
-                                </span>
-                              </>
-                            )
-                          }}
-                        />
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            )
-          })}
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={true}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          {listaCarousel.map(({ href, image, isRounded }, index) => (
+            <CarouselItem key={index} href={href} image={image} isRounded={isRounded} />
+          ))}
         </Carousel>
       ) : null}
     </Loading>
