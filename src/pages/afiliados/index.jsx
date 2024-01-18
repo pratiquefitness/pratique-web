f3e4bd20b56d5d67be54f7956bd07544f798a3f1
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-01-18 14:56:28 -0300

    Update index.jsx

--- src/pages/afiliados/index.jsx
+++ src/pages/afiliados/index.jsx
@@ -1,34 +1,39 @@
 import { Tabs } from 'antd'
-import Geral from './_Geral'
-import Comissao from './_Comissao'
-import Produtos from './_Produtos'
-import PlanosAcademia from './_PlanosAcademia'
-import JumperFit from './_JumperFit'
-import PratiqueMed from './_PratiqueMed'
-
-const items = [
-  {
-    key: 'geral',
-    label: `Geral`,
-    children: <Geral />
-  },
-  {
-    key: 'comissao',
-    label: `Comissão`,
-    children: <Comissao />
-  },
-  {
-    key: 'planos',
-    label: `Planos`,
-    children: <PlanosAcademia />
-  },
-  {
-    key: 'jumperfit',
-    label: `Jumper Fit`,
-    children: <JumperFit />
-  }
-]
+import Produtos from '../_Produtos'
+import PlanosAcademia from '../_PlanosAcademia'
+import { useRouter } from 'next/router'
 
 export default function Afiliados() {
-  return <Tabs defaultActiveKey="0" items={items} />
+  const router = useRouter()
+  const { employee } = router.query
+
+  return (
+    <div className="p-5">
+      <Tabs
+        defaultActiveKey="0"
+        items={[
+          {
+            key: 'produtos',
+            label: `Produtos`,
+            children: <Produtos employee={employee} />
+          },
+          {
+            key: 'planos',
+            label: `Planos`,
+            children: <PlanosAcademia employee={employee} />
+          },
+          {
+            key: 'jumperfit',
+            label: `Jumper Fit`,
+            children: <JumperFit employee={employee} />
+          },
+          {
+            key: 'pratiquemed',
+            label: `Pratique Med`,
+            children: <PratiqueMed employee={employee} />
+          }
+        ]}
+      />
+    </div>
+  )
 }
