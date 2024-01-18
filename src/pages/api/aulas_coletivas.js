8c76f822eaa0c53e0eee589a934ed85ef1a4b4f0
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-01-18 18:43:18 -0300

    Update aulas_coletivas.js

--- src/pages/api/aulas_coletivas.js
+++ src/pages/api/aulas_coletivas.js
@@ -1,11 +1,14 @@
-import { apiPratiqueAulas } from '@/services'
+import { apiPratiqueAulas } from "@/services";
 
 export default async function handler(req, res) {
-  const { aula_categoria } = req.body
+  const { aula_categoria } = req.body;
   const data = await apiPratiqueAulas.aulas.findMany({
     where: {
-      aula_categoria: String(aula_categoria)
-    }
-  })
-  res.status(200).json(data)
+      aula_categoria: String(aula_categoria),
+    },
+    orderBy: {
+      aula_data: "desc",
+    },
+  });
+  res.status(200).json(data);
 }
