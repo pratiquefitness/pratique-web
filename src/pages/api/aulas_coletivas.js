6b7eb88f8db49a4df6373be01d40141f52cbd90d
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-01-18 23:47:36 -0300

    Update aulas_coletivas.js

--- src/pages/api/aulas_coletivas.js
+++ src/pages/api/aulas_coletivas.js
@@ -2,17 +2,24 @@ import { apiPratiqueAulas } from "@/services";
 import { format } from "date-fns";
 
 export default async function handler(req, res) {
-  const { aula_categoria } = req.body;
-  const data = await apiPratiqueAulas.aulas.findMany({
-    where: {
-      aula_datagravacao: {
-        gte: format(new Date(), "yyyy-MM-dd"),
+  try {
+    const { aula_categoria } = req.body;
+
+    const data = await apiPratiqueAulas.aulas.findMany({
+      where: {
+        aula_datagravacao: {
+          gte: format(new Date(), "yyyy-MM-dd"),
+        },
+        aula_categoria: String(aula_categoria),
       },
-      aula_categoria: String(aula_categoria), // Colocado dentro de la cláusula where
-    },
-    orderBy: {
-      aula_datagravacao: "desc",
-    },
-  });
-  res.status(200).json(data);
+      orderBy: {
+        aula_datagravacao: "desc",
+      },
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Erro na consulta ao banco de dados:", error);
+    res.status(500).json({ error: "Erro interno no servidor" });
+  }
 }
