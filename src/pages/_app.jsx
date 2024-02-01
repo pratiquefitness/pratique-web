bf7a2d569b42208b2a90796efe2045f8263a5411
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-02-01 00:42:15 -0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,20 +5,23 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import '@/styles/globals.css'
 import Head from 'next/head'
 
-// Adicione os scripts do OneSignal no Head do seu aplicativo
 const OneSignalScript = () => (
   <>
     <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-    <script>
-      var OneSignal = window.OneSignal || [];
-      OneSignal.push(function() {
-        OneSignal.init({
-          appId: 'fef4bca8-b29c-43b2-8587-4447f7dbc98b',
-        });
-      });
-    </script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          var OneSignal = window.OneSignal || [];
+          OneSignal.push(function() {
+            OneSignal.init({
+              appId: 'fef4bca8-b29c-43b2-8587-4447f7dbc98b',
+            });
+          });
+        `
+      }}
+    />
   </>
-);
+)
 
 export default function App({ Component, pageProps }) {
   return (
@@ -27,7 +30,6 @@ export default function App({ Component, pageProps }) {
         <title>Pratique em Casa</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        {/* Adicione o componente OneSignalScript ao Head */}
         <OneSignalScript />
       </Head>
       <Provider store={store}>
@@ -38,5 +40,5 @@ export default function App({ Component, pageProps }) {
         </AuthProvider>
       </Provider>
     </>
-  );
+  )
 }
