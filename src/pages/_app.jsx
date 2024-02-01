c71e7bd2ff4384ce3d79fc225d654de24ec45221
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-02-01 00:38:04 -0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,21 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import '@/styles/globals.css'
 import Head from 'next/head'
 
+// Adicione os scripts do OneSignal no Head do seu aplicativo
+const OneSignalScript = () => (
+  <>
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+      var OneSignal = window.OneSignal || [];
+      OneSignal.push(function() {
+        OneSignal.init({
+          appId: 'fef4bca8-b29c-43b2-8587-4447f7dbc98b',
+        });
+      });
+    </script>
+  </>
+);
+
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -12,6 +27,8 @@ export default function App({ Component, pageProps }) {
         <title>Pratique em Casa</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+        {/* Adicione o componente OneSignalScript ao Head */}
+        <OneSignalScript />
       </Head>
       <Provider store={store}>
         <AuthProvider>
@@ -21,5 +38,5 @@ export default function App({ Component, pageProps }) {
         </AuthProvider>
       </Provider>
     </>
-  )
+  );
 }
