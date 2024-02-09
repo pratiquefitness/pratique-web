3f4b3e75ac7213a4886d888bde0bf7682fd4bb86
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-02-09 03:14:51 -0300

    Update cis.js

--- src/redux/actions/cis.js
+++ src/redux/actions/cis.js
@@ -3,15 +3,24 @@ import api from '@/services/api'
 
 export const getCis = () => {
   return async (dispatch, getState) => {
-    const { login } = getState()
-    dispatch(setLoading(true))
-    return api
-      .post('/cis', { email: login.usuario.user_email, cargo: login.cargo })
-      .then(res => {
-        dispatch(setData(res.data))
+    try {
+      const { login } = getState()
+      // console.log('Estado atual de login:', login)
+      dispatch(setLoading(true))
+
+      const res = await api.post('/cis', {
+        email: login.usuario.user_email,
+        cargo: login.usuario.cargo
       })
-      .finally(() => {
-        dispatch(setLoading(false))
-      })
+
+      // console.log('Resposta da API:', res.data)
+
+      dispatch(setData(res.data))
+    } catch (error) {
+      // console.error('Erro ao obter cis:', error)
+    } finally {
+      // console.log('Finalizando requisição')
+      dispatch(setLoading(false))
+    }
   }
 }
