e81df1b743400e668acbf62f7d793f0bfd21c1b0
renatochristian <renatochristian@outlook.com>
2024-02-13 00:01:01 -0300

    envio diagnose

--- src/pages/api/envioDiagnose.js
+++ src/pages/api/envioDiagnose.js
@@ -1,28 +1,31 @@
-import apiPratiqueTecnologia from '@/services/apiPratiqueTecnologia';
+import apiPratiqueTecnologia from '@/services/apiPratiqueTecnologia'
 
 export default async function apiPratiqueTec(req, res) {
   if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Método não permitido' });
-    return;
+    res.status(405).json({ message: 'Método não permitido' })
+    return
   }
 
   try {
-	const respostaArray = JSON.parse(Object.keys(req.body)[0]);
+    const respostaArray = JSON.parse(Object.keys(req.body)[0])
+    const stringResposta = JSON.stringify(respostaArray)
 
+    const apiUrl = '/app/diagnose/envia/processa/respostas.php'
+    //const urlWithParams = `${apiUrl}?resposta=${stringResposta}`
+
+	//console.log("url final",urlWithParams)
     const response = await apiPratiqueTecnologia.post(
-      '/app/diagnose/envia/processa/respostas.php',
-      { resposta: respostaArray },
+      apiUrl,
+      { resposta: stringResposta },
       {
         headers: {
-          'Content-Type': 'application/json',
-        },
+          'Content-Type': 'application/json'
+        }
       }
-    );
-    res.status(200).json(response.data);
+    )
+    res.status(200).json(response.data)
   } catch (error) {
-    console.log('error', error);
-    res
-      .status(error.response?.status || 500)
-      .json(error.response?.data || { message: 'Internal Server Error' });
+    console.log('error', error)
+    res.status(error.response?.status || 500).json(error.response?.data || { message: 'Internal Server Error' })
   }
 }
