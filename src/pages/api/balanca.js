702722664a83fa816d4c9ac3ddbcd7f2297291e9
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-03-25 22:05:38 -0300

    Update balanca.js

--- src/pages/api/balanca.js
+++ src/pages/api/balanca.js
@@ -1,30 +1,76 @@
-const express = require('express')
-const fetch = require('node-fetch')
-const app = express()
+import axios from 'axios'
 
-// Endpoint para lidar com a solicitação de download do PDF
-app.get('/download-pdf', async (req, res) => {
-  const { examId } = req.query
+const API_URL = 'https://academiapratique.com.br/casuais/balanca/verifica.php'
+const IMAGE_URL = 'https://academiapratique.com.br/casuais/balanca/imagem.php'
+
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(API_URL)
+    return response.data
+  } catch (error) {
+    console.error('Erro ao chamar a API:', error)
+    throw new Error('Falha na solicitação.')
+  }
+}
+
+export const fetchImageKey = async (apiKey, phone) => {
+  const params = {
+    apiKey: apiKey,
+    phone: phone
+  }
+
+  const queryString = new URLSearchParams(params).toString()
 
   try {
-    // Faz a solicitação ao servidor externo para obter o PDF
-    const response = await fetch(`https://www.anovator.com/report/index.html?id=${examId}&child=false&lang=en_EN`)
-    const pdfBlob = await response.blob()
+    const response = await axios.get(`${API_URL}?${queryString}`)
+    const data = response.data
 
-    // Define os cabeçalhos da resposta
-    res.setHeader('Content-Disposition', 'attachment; filename=report.pdf')
-    res.setHeader('Content-Type', 'application/pdf')
-
-    // Envia o blob do PDF como resposta
-    pdfBlob.stream().pipe(res)
+    if (data && data.DATA && data.DATA.length > 0) {
+      for (const item of data.DATA) {
+        if (item.bodyImage) {
+          console.log(item.bodyImage)
+          return item.bodyImage
+        }
+      }
+      throw new Error('Nenhuma chave de imagem encontrada nos dados da resposta.')
+    } else {
+      throw new Error('Nenhum dado encontrado na resposta da API.')
+    }
   } catch (error) {
-    console.error('Erro ao baixar o relatório PDF:', error)
-    res.status(500).send('Erro ao baixar o relatório PDF')
+    console.error('Erro ao buscar a chave da imagem:', error)
+    throw new Error('Falha ao buscar a chave da imagem.')
   }
-})
+}
 
-// Inicia o servidor na porta 3001
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Servidor está ouvindo na porta ${PORT}`)
-})
+export const fetchAndDisplayImage = async (apiKey, imageKey) => {
+  try {
+    const response = await axios.get(IMAGE_URL, {
+      params: {
+        key: imageKey
+      },
+      responseType: 'blob'
+    })
+
+    const imageUrlObject = URL.createObjectURL(new Blob([response.data]))
+
+    const imageElement = document.createElement('img')
+    imageElement.src = imageUrlObject
+    document.body.appendChild(imageElement)
+  } catch (error) {
+    console.error('Erro ao exibir a imagem:', error)
+    throw new Error('Falha ao exibir a imagem.')
+  }
+}
+
+export const generatePDF = async phone => {
+  try {
+    // Busca a chave de imagem com base no telefone do usuário
+    const imageKey = await fetchImageKey(phone)
+
+    // Abre o link do relatório em uma nova janela
+    window.open(`https://www.anovator.com/report/index.html?id=${imageKey}&child=false&lang=en_EN`, '_blank')
+  } catch (error) {
+    console.error('Erro ao gerar o PDF do relatório:', error)
+    throw new Error('Falha ao gerar o PDF do relatório.')
+  }
+}
