3b23d6d747c6297d2391f7e45e40f41d08fc46ce
pratiquefitness <pratiquefitness@gmail.com>
2024-04-03 14:35:08 -0300

    novo

--- src/pages/api/diagnose.js
+++ src/pages/api/diagnose.js
@@ -5,13 +5,11 @@ export default async function handler(req, res) {
     const { email } = req.body
 
     // Adicione uma verificação para garantir que apiPratiqueDiagnose.sva seja definido
-    if (!apiPratiqueDiagnose || !apiPratiqueDiagnose.windows) {
-      return res
-        .status(500)
-        .json({ error: 'Internal Server Error', details: 'apiPratiqueDiagnose.windows is undefined' })
+    if (!apiPratiqueDiagnose || !apiPratiqueDiagnose.sva) {
+      return res.status(500).json({ error: 'Internal Server Error', details: 'apiPratiqueDiagnose.sva is undefined' })
     }
 
-    const data = await apiPratiqueDiagnose.windows.findMany({
+    const data = await apiPratiqueDiagnose.sva.findMany({
       where: {
         diagnose_email: email
       },
@@ -19,9 +17,8 @@ export default async function handler(req, res) {
         diagnose_data: 'desc'
       }
     })
-	  
-	  
-	  // Adicione uma verificação para garantir que apiPratiqueDiagnose.sva seja definido
+
+    // Adicione uma verificação para garantir que apiPratiqueDiagnose.sva seja definido
     /*if (!apiPratiqueDiagnose || !apiPratiqueDiagnose.sva) {
       return res.status(500).json({ error: 'Internal Server Error', details: 'apiPratiqueDiagnose.sva is undefined' })
     }
@@ -34,8 +31,6 @@ export default async function handler(req, res) {
         diagnose_data: 'desc'
       }
     })*/
-	  
-	  
 
     // Converta o campo diagnose_data para string
     const formattedData = data.map(entry => ({
