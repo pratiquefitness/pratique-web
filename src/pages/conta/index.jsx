e15915449ec606a42e2a5b2e0d788a094e78795c
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-06-06 02:51:08 -0300

    Update index.jsx

--- src/pages/conta/index.jsx
+++ src/pages/conta/index.jsx
@@ -1,40 +1,49 @@
-import { Tabs } from 'antd';
-import Dados from './_Dados';
-import Plano from './_Plano';
-import AlunosPersonal from './_AlunosPersonal';
-import AlunosTreino from './_AlunosTreino';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getAlunosPersonal } from '@/redux/actions/conta';
+import { Tabs } from 'antd'
+import Dados from './_Dados'
+import Plano from './_Plano'
+import FAQ from './_Faq'
+import AlunosPersonal from './_AlunosPersonal'
+import AlunosTreino from './_AlunosTreino'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getAlunosPersonal } from '@/redux/actions/conta'
 import CriarTreinosAluno from './_CriarTreinosAluno'
 
 export default function Conta() {
-  const dispatch = useDispatch();
-  const { usuario } = useSelector(state => state.login);
+  const dispatch = useDispatch()
+  const { usuario } = useSelector(state => state.login)
 
   useEffect(() => {
-    dispatch(getAlunosPersonal());
-  }, []);
+    dispatch(getAlunosPersonal())
+  }, [dispatch])
 
   const items = [
     {
       key: 'dados',
       label: `Dados`,
       children: <Dados />
-    },{
+    },
+    {
       key: 'plano',
       label: `Plano`,
       children: <Plano />
-    },{
+    },
+    {
+      key: 'faq',
+      label: `FAQ`,
+      children: <FAQ />
+    },
+    {
       key: usuario.professor === 1 ? 'alunos' : '',
       label: usuario.professor === 1 ? `Alunos` : '',
       children: usuario.professor === 1 ? <AlunosPersonal /> : ''
-    },{
+    },
+    {
       key: usuario.professor === 1 ? 'treinoAlunos' : '',
       label: usuario.professor === 1 ? `Criar treinos` : '',
       children: usuario.professor === 1 ? <CriarTreinosAluno /> : ''
     }
-  ];
+  ]
 
-  return <Tabs defaultActiveKey="1" items={items} />
+  return <Tabs defaultActiveKey="dados" items={items} />
 }
