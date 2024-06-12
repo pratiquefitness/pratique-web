275c823c57aa1d39d4deee711dbd440a8ce03101
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-06-12 19:12:39 -0300

    Update _CarouselItemPersonal.jsx

--- src/pages/area_personal/_CarouselItemPersonal.jsx
+++ src/pages/area_personal/_CarouselItemPersonal.jsx
@@ -1,33 +1,67 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function CarouselItemPersonal({
- image,
- alt,
- isRounded,
- curriculo,
- telefone,
- href,
- displayName,
+  image,
+  alt,
+  isRounded,
+  curriculo,
+  telefone,
+  href,
+  displayName,
   id,
- setImageModal,
- setExercicioModal
+  setImageModal,
+  setExercicioModal
 }) {
   const router = useRouter()
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 464)
+    }
+
+    handleResize() // Verifica o tamanho da janela na montagem do componente
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const handleClick = () => {
-    router.push({
-      pathname: href,
-      query: { curriculo: curriculo, telefone: telefone, image: image, displayName: displayName}
-    }, href)
+    router.push(
+      {
+        pathname: href,
+        query: { curriculo, telefone, image, displayName }
+      },
+      href
+    )
   }
 
   return (
     <a onClick={handleClick} className="d-flex justify-center">
-      <Image
-        width={300}
-        height={300}
-        src={image} alt={alt} className={`w-95 ${isRounded && 'rounded-extra'}`} />
+      <div className="image-wrapper">
+        <Image
+          src={image}
+          alt={alt}
+          layout="fill"
+          className={`w-95 ${isRounded ? 'rounded-extra' : ''}`}
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <style jsx>{`
+        .image-wrapper {
+          position: relative;
+          width: 100%;
+          height: ${isMobile ? '150px' : '300px'}; // 300px ou qualquer altura desejada para desktop
+        }
+        .rounded-extra {
+          border-radius: 50%;
+        }
+      `}</style>
     </a>
   )
 }
