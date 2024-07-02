ffb35530248ff079a9c83a894a4bed2cc2722f94
jefferson <jefferson.ls27@gmail.com>
2024-07-02 20:33:13 -0300

    Botão whatsapp

--- src/pages/contato_personal/index.jsx
+++ src/pages/contato_personal/index.jsx
@@ -7,6 +7,11 @@ import { FaWhatsapp } from 'react-icons/fa'
 
 export default function Contato() {
   const router = useRouter()
+
+  const handleOpenWhatsapp = () => {
+    router.push(`https://api.whatsapp.com/send?phone=55${router.query.telefone}&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20gostaria%20de%20falar%20sobre%20seu%20personal.`);
+  };
+
   return (
     <>
       {
@@ -29,14 +34,17 @@ export default function Contato() {
       }
 
       <Space direction="vertical" className="w-100 mt-6">
-        <a
+        <Button icon={<FaWhatsapp fill="#25D366" />} block onClick={handleOpenWhatsapp}>
+          Entre em contato comigo
+        </Button>
+        {/*<a
           href={`https://api.whatsapp.com/send?phone=55${router.query.telefone}&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20gostaria%20de%20falar%20sobre%20seu%20personal.`}
           target="_blank"
         >
           <Button icon={<FaWhatsapp fill="#25D366" />} block>
             Entre em contato comigo
           </Button>
-        </a>
+        </a>*/}
       </Space>
     </>
   )
