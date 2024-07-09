4a4a5b7b8743a13e3712afc5dff61a32eb028a1e
jefferson <jefferson.ls27@gmail.com>
2024-07-09 08:47:58 -0300

    Whatsapp

--- src/pages/contato_personal/index.jsx
+++ src/pages/contato_personal/index.jsx
@@ -10,18 +10,6 @@ export default function Contato() {
   const router = useRouter();
   const isMobile = typeof window !== 'undefined' && /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)
 
-  const handleOpenWhatsapp = () => {
-    const whatsappUrl =
-      `https://api.whatsapp.com/send?phone=55${router.query.telefone}&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20gostaria%20de%20falar%20sobre%20seu%20personal.`
-
-    if (isMobile) {
-      window.location.href = whatsappUrl
-      return
-    }
-
-    window.open(whatsappUrl, '_blank')
-  }
-
   return (
     <>
       {
@@ -67,17 +55,14 @@ export default function Contato() {
       }
 
       <Space direction="vertical" className="w-100 mt-6">
-        <Button icon={<FaWhatsapp fill="#25D366" />} block onClick={handleOpenWhatsapp}>
-          Entre em contato comigo
-        </Button>
-        {/*<a
-          href={`https://api.whatsapp.com/send?phone=55${router.query.telefone}&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20gostaria%20de%20falar%20sobre%20seu%20personal.`}
+        <a
+          href={`https://api.whatsapp.com/send?phone=55${router.query.telefone.replace(/[^0-9a-zA-Z]/g, "")}&text=Ol%C3%A1%20estou%20no%20Aplicativo%20Pratique%20em%20Casa%20e%20gostaria%20de%20falar%20sobre%20seu%20personal.`}
           target="_blank"
         >
           <Button icon={<FaWhatsapp fill="#25D366" />} block>
             Entre em contato comigo
           </Button>
-        </a>*/}
+        </a>
       </Space>
     </>
   )
