015fa6c3693ea18e94174fa469d367e3dedcdc75
Academia Pratique Fitness <102559877+pratiquefitness@users.noreply.github.com>
2024-07-13 10:38:53 -0300

    Update _AtividadesOnDemand.jsx

--- src/pages/inicio/_AtividadesOnDemand.jsx
+++ src/pages/inicio/_AtividadesOnDemand.jsx
@@ -1,60 +1,64 @@
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
-import CarouselItem from './_CarouselItem'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { useSelector } from 'react-redux'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import CarouselItem from "./_CarouselItem";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useSelector } from "react-redux";
 
 const listaCarousel = [
   {
-    href: '/aulas_coletivas/powerdance',
-    image: '/images/webp/banner_home/vertical_banner/power-dance-vertical.webp',
+    href: "/aulas_coletivas/coreografias",
+    image: "/images/webp/banner_home/vertical_banner/coreografias-vertical.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/powercore',
-    image: '/images/webp/banner_home/vertical_banner/power-core-vertical.webp',
+    href: "/aulas_coletivas/powerdance",
+    image: "/images/webp/banner_home/vertical_banner/power-dance-vertical.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/powerbumbum',
-    image: '/images/webp/banner_home/vertical_banner/power_bumbum2.webp',
+    href: "/aulas_coletivas/powercore",
+    image: "/images/webp/banner_home/vertical_banner/power-core-vertical.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/treinosdiarios',
-    image: '/images/webp/banner_home/vertical_banner/treinos_diarios.webp',
+    href: "/aulas_coletivas/powerbumbum",
+    image: "/images/webp/banner_home/vertical_banner/power_bumbum2.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/abdominais',
-    image: '/images/webp/banner_home/vertical_banner/abdominais.webp',
+    href: "/aulas_coletivas/treinosdiarios",
+    image: "/images/webp/banner_home/vertical_banner/treinos_diarios.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/flow',
-    image: '/images/webp/banner_home/vertical_banner/flow.webp',
+    href: "/aulas_coletivas/abdominais",
+    image: "/images/webp/banner_home/vertical_banner/abdominais.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/queime_em_casa',
-    image: '/images/webp/banner_home/vertical_banner/queime_casa.webp',
+    href: "/aulas_coletivas/flow",
+    image: "/images/webp/banner_home/vertical_banner/flow.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/jump',
-    image: '/images/webp/banner_home/vertical_banner/energy_jump2.webp',
+    href: "/aulas_coletivas/queime_em_casa",
+    image: "/images/webp/banner_home/vertical_banner/queime_casa.webp",
     isRounded: true
   },
   {
-    href: '/aulas_coletivas/intensivejump',
-    image: '/images/webp/banner_home/vertical_banner/intensive_jump.webp',
+    href: "/aulas_coletivas/jump",
+    image: "/images/webp/banner_home/vertical_banner/energy_jump2.webp",
+    isRounded: true
+  },
+  {
+    href: "/aulas_coletivas/intensivejump",
+    image: "/images/webp/banner_home/vertical_banner/intensive_jump.webp",
     isRounded: true
   }
-]
+];
 
 export default function AtividadesOnDemand() {
-
   return (
     <Carousel
       additionalTransfrom={0}
@@ -101,5 +105,5 @@ export default function AtividadesOnDemand() {
         <CarouselItem key={index} href={href} image={image} isRounded={isRounded} />
       ))}
     </Carousel>
-  )
+  );
 }
