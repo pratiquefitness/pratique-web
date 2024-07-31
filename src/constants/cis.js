ba79cc1d4326f257c5e831c9f40fdef806df38c7
pratiquefitness <pratiquefitness@gmail.com>
2024-07-31 09:43:46 -0300

    Atualizações no arquivo cis.js

--- src/constants/cis.js
+++ src/constants/cis.js
@@ -1,335 +1,339 @@
 const cis = [
   {
-    cargo: 'ADMINISTRAÇÃO',
-    ci: ['CI ADMINISTRATIVO']
+    cargo: "ADMINISTRAÇÃO",
+    ci: ["CI ADMINISTRATIVO"]
   },
   {
-    cargo: 'SAC',
-    ci: ['CI SAC']
+    cargo: "ADMINISTRAÃ‡ÃƒO",
+    ci: ["CI ADMINISTRATIVO"]
   },
   {
-    cargo: 'SECRETARIA',
-    ci: ['CI SECRETARIA']
+    cargo: "SAC",
+    ci: ["CI SAC"]
   },
   {
-    cargo: 'MUSCULAÇÃO',
-    ci: ['CI MUSCULACAO']
+    cargo: "SECRETARIA",
+    ci: ["CI SECRETARIA"]
   },
   {
-    cargo: 'MUSCULAÃ‡ÃƒO',
-    ci: ['CI MUSCULACAO']
+    cargo: "MUSCULAÇÃO",
+    ci: ["CI MUSCULACAO"]
   },
   {
-    cargo: 'LIMPEZA',
-    ci: ['CI LIMPEZA']
+    cargo: "MUSCULAÃ‡ÃƒO",
+    ci: ["CI MUSCULACAO"]
   },
   {
-    cargo: 'COLETIVAS',
-    ci: ['CI COLETIVAS']
+    cargo: "LIMPEZA",
+    ci: ["CI LIMPEZA"]
   },
   {
-    cargo: 'ZUMBA',
-    ci: ['CI COLETIVAS']
+    cargo: "COLETIVAS",
+    ci: ["CI COLETIVAS"]
   },
   {
-    cargo: 'COORDENAÇÃO(CONFIANÇA)',
+    cargo: "ZUMBA",
+    ci: ["CI COLETIVAS"]
+  },
+  {
+    cargo: "COORDENAÇÃO(CONFIANÇA)",
     ci: [
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'COORDENACAO(CONFIANCA)',
+    cargo: "COORDENACAO(CONFIANCA)",
     ci: [
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'COORDENACAO CONFIANCA',
+    cargo: "COORDENACAO CONFIANCA",
     ci: [
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'GINASTICA',
-    ci: ['CI GINASTICA', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "GINASTICA",
+    ci: ["CI GINASTICA", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'GINÁSTICA',
-    ci: ['CI GINASTICA', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "GINÁSTICA",
+    ci: ["CI GINASTICA", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'RITMOS',
-    ci: ['CI RITMOS', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "RITMOS",
+    ci: ["CI RITMOS", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'ARTES MARCIAIS',
-    ci: ['CI ARTES MARCIAIS', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "ARTES MARCIAIS",
+    ci: ["CI ARTES MARCIAIS", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'COORDENADOR(GERÊNCIA)',
+    cargo: "COORDENADOR(GERÊNCIA)",
     ci: [
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'PISCINA',
-    ci: ['CI PISCINA', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "PISCINA",
+    ci: ["CI PISCINA", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'PERSONAL CLASS',
-    ci: ['CI PERSONAL']
+    cargo: "PERSONAL CLASS",
+    ci: ["CI PERSONAL"]
   },
   {
-    cargo: 'ZELADOR',
-    ci: ['CI ZELADOR']
+    cargo: "ZELADOR",
+    ci: ["CI ZELADOR"]
   },
   {
-    cargo: 'MUSCULACAO',
-    ci: ['CI MUSCULACAO', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "MUSCULACAO",
+    ci: ["CI MUSCULACAO", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'MUSCULAÇÃO',
-    ci: ['CI MUSCULACAO', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "MUSCULAÇÃO",
+    ci: ["CI MUSCULACAO", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'LOGISTICA',
-    ci: ['CI LOGISTICA']
+    cargo: "LOGISTICA",
+    ci: ["CI LOGISTICA"]
   },
   {
-    cargo: 'PRATIQUE EM CASA',
-    ci: ['CI PRATIQUE EM CASA']
+    cargo: "PRATIQUE EM CASA",
+    ci: ["CI PRATIQUE EM CASA"]
   },
   {
-    cargo: 'DEFESA',
-    ci: ['CI DEFESA', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "DEFESA",
+    ci: ["CI DEFESA", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'PILATES',
-    ci: ['CI PILATES', 'CI MUSCULACAO', 'CI COLETIVAS']
+    cargo: "PILATES",
+    ci: ["CI PILATES", "CI MUSCULACAO", "CI COLETIVAS"]
   },
   {
-    cargo: 'SECRETARIA(CONFIANÇA)',
-    ci: ['CI SECRETARIA']
+    cargo: "SECRETARIA(CONFIANÇA)",
+    ci: ["CI SECRETARIA"]
   },
   {
-    cargo: 'ADMINISTRAÇÃO',
+    cargo: "ADMINISTRAÇÃO",
     ci: [
-      'CI ADMINISTRATIVO',
-      'CI CONFIANCA',
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI ADMINISTRATIVO",
+      "CI CONFIANCA",
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'MANUTENÇÃO',
-    ci: ['CI MANUTENÇÃO']
+    cargo: "MANUTENÇÃO",
+    ci: ["CI MANUTENÇÃO"]
   },
   {
-    cargo: 'MARKETING',
+    cargo: "MARKETING",
     ci: [
-      'CI ADMINISTRATIVO',
-      'CI CONFIANCA',
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI ADMINISTRATIVO",
+      "CI CONFIANCA",
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'ADMINISTRATIVO',
+    cargo: "ADMINISTRATIVO",
     ci: [
-      'CI ADMINISTRATIVO',
-      'CI CONFIANCA',
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI ADMINISTRATIVO",
+      "CI CONFIANCA",
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'SUPERVISAO',
+    cargo: "SUPERVISAO",
     ci: [
-      'CI SUPERVISAO',
-      'CI ADMINISTRATIVO',
-      'CI CONFIANCA',
-      'CI DEFESA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI SUPERVISAO',
-      'CI ZELADOR'
+      "CI SUPERVISAO",
+      "CI ADMINISTRATIVO",
+      "CI CONFIANCA",
+      "CI DEFESA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI SUPERVISAO",
+      "CI ZELADOR"
     ]
   },
 
   {
-    cargo: 'GESTOR',
+    cargo: "GESTOR",
     ci: [
-      'CI DEFESA',
-      'CI CONFIANCA',
-      'CI OBRIGATORIA',
-      'CI ARTES MARCIAIS',
-      'CI COLETIVAS',
-      'CI GINASTICA',
-      'CI LIMPEZA',
-      'CI LOGISTICA',
-      'CI MANUTENCAO',
-      'CI MARKETING',
-      'CI MUSCULACAO',
-      'CI PERSONAL',
-      'CI PILATES',
-      'CI PISCINA',
-      'CI PRATIQUE EM CASA',
-      'CI RITMOS',
-      'CI SAC',
-      'CI SECRETARIA',
-      'CI ZELADOR'
+      "CI DEFESA",
+      "CI CONFIANCA",
+      "CI OBRIGATORIA",
+      "CI ARTES MARCIAIS",
+      "CI COLETIVAS",
+      "CI GINASTICA",
+      "CI LIMPEZA",
+      "CI LOGISTICA",
+      "CI MANUTENCAO",
+      "CI MARKETING",
+      "CI MUSCULACAO",
+      "CI PERSONAL",
+      "CI PILATES",
+      "CI PISCINA",
+      "CI PRATIQUE EM CASA",
+      "CI RITMOS",
+      "CI SAC",
+      "CI SECRETARIA",
+      "CI ZELADOR"
     ]
   },
   {
-    cargo: 'LIDER',
-    ci: ['CI MUSCULACAO', 'CI SECRETARIA', 'CI CONFIANCA']
+    cargo: "LIDER",
+    ci: ["CI MUSCULACAO", "CI SECRETARIA", "CI CONFIANCA"]
   },
   {
-    cargo: 'MENTOR NIVEL 1',
-    ci: ['CI MUSCULACAO', 'CI SECRETARIA', 'CI CONFIANCA']
+    cargo: "MENTOR NIVEL 1",
+    ci: ["CI MUSCULACAO", "CI SECRETARIA", "CI CONFIANCA"]
   },
   {
-    cargo: 'MENTOR NIVEL 2',
-    ci: ['CI MUSCULACAO', 'CI SECRETARIA', 'CI CONFIANCA']
+    cargo: "MENTOR NIVEL 2",
+    ci: ["CI MUSCULACAO", "CI SECRETARIA", "CI CONFIANCA"]
   },
   {
-    cargo: 'MENTOR NIVEL 3',
-    ci: ['CI MUSCULACAO', 'CI SECRETARIA', 'CI CONFIANCA']
+    cargo: "MENTOR NIVEL 3",
+    ci: ["CI MUSCULACAO", "CI SECRETARIA", "CI CONFIANCA"]
   },
   {
-    cargo: 'teste',
-    ci: ['CI TESTE']
-  },
-]
+    cargo: "teste",
+    ci: ["CI TESTE"]
+  }
+];
 
-export default cis
+export default cis;
