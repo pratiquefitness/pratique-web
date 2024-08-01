a4c542a3a4f9457c88d353435783a32cb9dbe394
pratiquefitness <pratiquefitness@gmail.com>
2024-08-01 10:24:55 -0300

    aulas coletivas

--- src/pages/aulas_coletivas/_AulasColetivas.jsx
+++ src/pages/aulas_coletivas/_AulasColetivas.jsx
@@ -1,33 +1,23 @@
-import React, { useEffect, useState } from "react";
+import { getAulasColetivas } from "@/redux/actions/aulasColetivas";
 import { Card, Col, Row, Tag } from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import modalidades from "@/constants/modalidades";
 import Loading from "@/components/Loading";
 import YoutubePlayer from "./_YoutubePlayer";
 import SproutPlayer from "./_SproutPlayer";
 
 export default function AulasColetivas({ tema }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [videoData, setVideoData] = useState(null);
+  const { data, loading } = useSelector((state) => state.aulasColetivas);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchAulas = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(`https://pratiquetecnologia.com.br/api/app/aulas/`);
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Erro ao buscar aulas:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAulas();
-  }, [tema]);
+    dispatch(getAulasColetivas(modalidades[tema] || 14));
+  }, [tema, dispatch]);
 
   const renderPlayer = () => {
-    if (videoData?.aula_categoria === "33") {
+    if (videoData.aula_categoria === "33") {
       return <SproutPlayer id={videoData.aula_sprout} onClose={() => setVideoData(null)} />;
     } else {
       return <YoutubePlayer id={videoData.aula_linkvideo} onClose={() => setVideoData(null)} />;
@@ -40,7 +30,7 @@ export default function AulasColetivas({ tema }) {
         <Row gutter={16}>
           {data.map(
             (aula, key) =>
-              aula.aula_capa.length > 0 && (
+              aula.aula_capa.length && (
                 <Col xs={{ span: 24 }} md={{ span: 12 }} key={key} className="pb-4">
                   <Card
                     title={aula.aula_nome}
@@ -49,7 +39,7 @@ export default function AulasColetivas({ tema }) {
                     style={{ cursor: "pointer" }}
                   >
                     <div style={{ position: "relative" }}>
-                      <img src={aula.aula_capa} width={"100%"} alt={aula.aula_nome} />
+                      <img src={aula.aula_capa} width={"100%"} />
                       <Tag
                         color={aula.aula_plano === "5" ? "#87d068" : "#2db7f5"}
                         style={{ position: "absolute", top: 30, right: 0 }}
