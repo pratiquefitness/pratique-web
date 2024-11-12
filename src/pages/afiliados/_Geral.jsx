bb3d1ad73722890354fdfdc569f1c971b6e89b1c
pratiquefitness <pratiquefitness@gmail.com>
2024-11-12 18:16:28 -0300

    pop

--- src/pages/afiliados/_Geral.jsx
+++ src/pages/afiliados/_Geral.jsx
@@ -1,180 +1,256 @@
-import { Loading } from "@/components";
-import { getDadosAfiliado, getPix, savePix } from "@/redux/actions/afiliados";
-import utils from "@/utils";
-import {
-  Button,
-  Card,
-  Col,
-  Form,
-  Input,
-  Row,
-  Select,
-  Space,
-  Statistic,
-  message,
-  theme
-} from "antd";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { WhatsAppOutlined } from "@ant-design/icons";
-import { FaWhatsapp } from "react-icons/fa";
+import { Loading } from '@/components'
+import { getDadosAfiliado, getPix, savePix } from '@/redux/actions/afiliados'
+import utils from '@/utils'
+import { Button, Card, Col, Form, Input, Modal, Row, Select, Statistic, message, theme } from 'antd'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { FaWhatsapp } from 'react-icons/fa'
 
 const tiposPix = [
-  { value: "cpf", label: "CPF" },
-  { value: "telefone", label: "Telefone" },
-  { value: "email", label: "E-mail" },
-  { value: "aleatoria", label: "Aleatória" }
-];
+  { value: 'cpf', label: 'CPF' },
+  { value: 'telefone', label: 'Telefone' },
+  { value: 'email', label: 'E-mail' },
+  { value: 'aleatoria', label: 'Aleatória' }
+]
 
 export default function Geral() {
-  const [editablePix, setEditablePix] = useState(false);
-  const [formPix] = Form.useForm();
-  const dispatch = useDispatch();
-  const { usuario } = useSelector((state) => state.login);
-  const { geral, loading, pix, pixLoading } = useSelector((state) => state.afiliados);
-  const { token } = theme.useToken();
+  const [editablePix, setEditablePix] = useState(false)
+  const [formPix] = Form.useForm()
+  const dispatch = useDispatch()
+  const { usuario } = useSelector(state => state.login)
+  const { geral, loading, pix, pixLoading } = useSelector(state => state.afiliados)
+  const { token } = theme.useToken()
 
   useEffect(() => {
-    dispatch(getDadosAfiliado());
-    dispatch(getPix());
-  }, []);
+    dispatch(getDadosAfiliado())
+    dispatch(getPix())
+  }, [])
 
   useEffect(() => {
-    formPix.setFieldsValue(pix);
-  }, [pix]);
+    if (pix) {
+      formPix.setFieldsValue(pix)
+    }
+  }, [pix])
 
   const onSavePix = ({ tipo, chave }) => {
-    dispatch(savePix(tipo, chave, setEditablePix));
-  };
+    dispatch(
+      savePix(tipo, chave, () => {
+        setEditablePix(false)
+        message.success('Pix salvo com sucesso!')
+      })
+    )
+  }
 
   const messageLink = () => {
-    message.success("Link copiado!");
-    utils.copyTextToClipboard(pix?.chave);
-  };
+    message.success('Chave copiada!')
+    utils.copyTextToClipboard(pix?.chave)
+  }
 
-  const isMobile =
-    typeof window !== "undefined" && /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
+  const isMobile = typeof window !== 'undefined' && /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)
 
   const openWhatsApp = () => {
     const whatsappUrl =
-      "https://api.whatsapp.com/send?phone=553135682676&text=Ol%C3%A1%2C%20estou%20vindo%20do%20bot%C3%A3o%20suporte%20ao%20AFILIADOS%20do%20super%20app%20PRATIQUE";
+      'https://api.whatsapp.com/send?phone=553135682676&text=Ol%C3%A1%2C%20estou%20vindo%20do%20bot%C3%A3o%20suporte%20ao%20AFILIADOS%20do%20super%20app%20PRATIQUE'
 
     if (isMobile) {
-      window.location.href = whatsappUrl;
-      return;
+      window.location.href = whatsappUrl
+      return
     }
 
-    window.open(whatsappUrl, "_blank");
-  };
+    window.open(whatsappUrl, '_blank')
+  }
+
+  // Verifica se o Pix está cadastrado
+  const pixCadastrado = pix && pix.chave
+
+  // Estado para controlar a visibilidade do modal
+  const [modalVisible, setModalVisible] = useState(false)
+
+  // Atualiza o estado do modal quando o Pix é carregado
+  useEffect(() => {
+    if (!pixLoading) {
+      if (pixCadastrado) {
+        setModalVisible(false)
+      } else {
+        setModalVisible(true)
+      }
+    }
+  }, [pixCadastrado, pixLoading])
 
   return (
-    <Loading spinning={loading}>
-      <Row gutter={[16, 16]} className="mb-4">
-        <Col span={5}>
-          <div
-            className="p-2"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="Vendas" an value={geral.totalVendas} />
-          </div>
-        </Col>{" "}
-        <Col span={5}>
-          <div
-            className="p-2"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="Balcão" value={geral.totalBalcao} />
-          </div>
-        </Col>
-        <Col span={5}>
-          <div
-            className="p-2"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="Afiliado" value={geral.totalAfiliados} />
-          </div>
-        </Col>
-        <Col span={4}>
-          <div
-            className="p-2"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="NPS" an value={geral.nps} />
-          </div>
-        </Col>
-        <Col span={5}>
-          <div
-            className="p-2"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="Tablet" an value={geral.tablet} />
-          </div>
-        </Col>
-        <Col span={12}>
-          <div
-            className="p-4"
-            style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}
-          >
-            <Statistic title="Vendas a Receber" value={geral.totalReceber} />
-          </div>
-        </Col>
-        <Col span={12}>
-          <div className="p-4" style={{ background: token.colorPrimary, borderRadius: 5 }}>
-            <Statistic
-              title={<span style={{ color: "white" }}>Valor a Receber</span>}
-              valueStyle={{ color: "white" }}
-              value={`R$ ${geral.valorReceber}`}
-            />
-          </div>
-        </Col>
-      </Row>
-      {/*
-      <Row className="mb-2">
-        <Col span={24}>
-          <a href={`https://app.pratiqueemcasa.com.br/afiliados/loja/${usuario.isAffiliate}`}>
-            <Button block>Veja aqui Sua Loja</Button>
-          </a>
-        </Col>
-      </Row>
-      */}
-      <Card title="Seu Pix">
-        {editablePix || pix.length ? (
+    <>
+      <Loading spinning={loading}>
+        {/* Renderiza o conteúdo somente após verificar se o Pix está carregado */}
+        {!pixLoading && (
+          <>
+            <Row gutter={[16, 16]} className="mb-4">
+              <Col span={5}>
+                <div className="p-2" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="Vendas" value={geral?.totalVendas || 0} />
+                </div>
+              </Col>
+              <Col span={5}>
+                <div className="p-2" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="Balcão" value={geral?.totalBalcao || 0} />
+                </div>
+              </Col>
+              <Col span={5}>
+                <div className="p-2" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="Afiliado" value={geral?.totalAfiliados || 0} />
+                </div>
+              </Col>
+              <Col span={4}>
+                <div className="p-2" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="NPS" value={geral?.nps || 0} />
+                </div>
+              </Col>
+              <Col span={5}>
+                <div className="p-2" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="Tablet" value={geral?.tablet || 0} />
+                </div>
+              </Col>
+              <Col span={12}>
+                <div className="p-4" style={{ background: token.colorBgContainerDisabled, borderRadius: 5 }}>
+                  <Statistic title="Vendas a Receber" value={geral?.totalReceber || 0} />
+                </div>
+              </Col>
+              <Col span={12}>
+                <div className="p-4" style={{ background: token.colorPrimary, borderRadius: 5 }}>
+                  <Statistic
+                    title={<span style={{ color: 'white' }}>Valor a Receber</span>}
+                    valueStyle={{ color: 'white' }}
+                    value={`R$ ${geral?.valorReceber || '0,00'}`}
+                  />
+                </div>
+              </Col>
+            </Row>
+            {/* Conteúdo adicional */}
+            <Card title="Seu Pix">
+              {editablePix ? (
+                <Form form={formPix} layout="vertical" onFinish={onSavePix}>
+                  <Row gutter={[16, 16]}>
+                    <Col xs={24} lg={8}>
+                      <Form.Item
+                        name="tipo"
+                        label="Tipo de Chave"
+                        rules={[{ required: true, message: 'Selecione o tipo de chave' }]}
+                      >
+                        <Select placeholder="Selecione..." options={tiposPix} loading={pixLoading} />
+                      </Form.Item>
+                    </Col>
+                    <Col xs={24} lg={8}>
+                      <Form.Item name="chave" label="Chave PIX" rules={[{ required: true, message: 'Digite a chave' }]}>
+                        <Input placeholder="Sua chave PIX" />
+                      </Form.Item>
+                    </Col>
+                    <Col xs={24} lg={8}>
+                      <Form.Item
+                        label="Confirme a chave PIX"
+                        name="rChave"
+                        dependencies={['chave']}
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Confirme a chave.'
+                          },
+                          ({ getFieldValue }) => ({
+                            validator(_, value) {
+                              if (!value || getFieldValue('chave') === value) {
+                                return Promise.resolve()
+                              }
+                              return Promise.reject(new Error('As chaves devem ser iguais.'))
+                            }
+                          })
+                        ]}
+                        hasFeedback
+                      >
+                        <Input placeholder="Confirme a chave PIX" />
+                      </Form.Item>
+                    </Col>
+                    <Col xs={24}>
+                      <Button type="primary" htmlType="submit" loading={pixLoading} block>
+                        Salvar
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              ) : (
+                <Row gutter={[16, 16]}>
+                  <Col xs={24}>
+                    Tipo da Chave: {pix?.tipo ? tiposPix.find(tipo => tipo.value === pix.tipo)?.label || 'N/A' : 'N/A'}
+                  </Col>
+                  <Col xs={24} lg={12} className="d-flex align-center justify-space-between">
+                    Chave: {pix?.chave || 'N/A'}
+                    <Button
+                      type="primary"
+                      style={{ background: '#1677ff' }}
+                      size="small"
+                      onClick={messageLink}
+                      disabled={!pix?.chave}
+                    >
+                      Copiar Chave
+                    </Button>
+                  </Col>
+                  <Col xs={24}>
+                    <Button onClick={() => setEditablePix(true)} block>
+                      Editar
+                    </Button>
+                    <br />
+                    <p className="my-2 text-center">Precisa de apoio?</p>
+                    <Col xs={24} className="mb-12">
+                      <Button
+                        icon={<FaWhatsapp fill="#fff" size={30} />}
+                        style={{ background: 'green', color: 'white' }}
+                        block
+                        onClick={openWhatsApp}
+                      >
+                        SUPORTE AFILIADO
+                      </Button>
+                      <p className="my-2 text-center">(31) 3568-2676</p>
+                    </Col>
+                  </Col>
+                </Row>
+              )}
+            </Card>
+          </>
+        )}
+      </Loading>
+
+      {/* Modal para cadastro do Pix */}
+      {!pixLoading && (
+        <Modal title="Cadastre seu Pix" visible={modalVisible} closable={false} footer={null} maskClosable={false}>
           <Form form={formPix} layout="vertical" onFinish={onSavePix}>
             <Row gutter={[16, 16]}>
-              <Col xs={24} lg={8}>
+              <Col xs={24} lg={24}>
                 <Form.Item
                   name="tipo"
                   label="Tipo de Chave"
-                  rules={[{ required: true, message: "Selecione o tipo de chave" }]}
+                  rules={[{ required: true, message: 'Selecione o tipo de chave' }]}
                 >
                   <Select placeholder="Selecione..." options={tiposPix} loading={pixLoading} />
                 </Form.Item>
               </Col>
-              <Col xs={24} lg={8}>
-                <Form.Item
-                  name="chave"
-                  label="Chave PIX"
-                  rules={[{ required: true, message: "Digite a chave" }]}
-                >
+              <Col xs={24} lg={24}>
+                <Form.Item name="chave" label="Chave PIX" rules={[{ required: true, message: 'Digite a chave' }]}>
                   <Input placeholder="Sua chave PIX" />
                 </Form.Item>
               </Col>
-              <Col xs={24} lg={8}>
+              <Col xs={24} lg={24}>
                 <Form.Item
                   label="Confirme a chave PIX"
                   name="rChave"
-                  dependencies={["chave"]}
+                  dependencies={['chave']}
                   rules={[
                     {
                       required: true,
-                      message: "Confirme a chave."
+                      message: 'Confirme a chave.'
                     },
                     ({ getFieldValue }) => ({
                       validator(_, value) {
-                        if (!value || getFieldValue("chave") === value) {
-                          return Promise.resolve();
+                        if (!value || getFieldValue('chave') === value) {
+                          return Promise.resolve()
                         }
-                        return Promise.reject(new Error("As chaves devem ser iguais."));
+                        return Promise.reject(new Error('As chaves devem ser iguais.'))
                       }
                     })
                   ]}
@@ -190,44 +266,8 @@ export default function Geral() {
               </Col>
             </Row>
           </Form>
-        ) : (
-          <Row gutter={[16, 16]}>
-            <Col xs={24}>
-              Tipo da Chave: {tiposPix.find((tipo) => tipo.value === pix.tipo)?.label}
-            </Col>
-            <Col xs={24} lg={12} className="d-flex align-center justify-space-between">
-              Chave: {pix?.chave}
-              <Button
-                type="primary"
-                style={{ background: "#1677ff" }}
-                size="small"
-                onClick={messageLink}
-              >
-                Copiar Chave
-              </Button>
-            </Col>
-            <Col xs={24}>
-              <Button onClick={() => setEditablePix(true)} block>
-                Editar
-              </Button>
-              <br />
-              <p className="my-2 text-center">Precisa de apoio?</p>
-              <Col xs={24} className="mb-12">
-                <Button
-                  icon={<FaWhatsapp fill="#fff" size={30} />}
-                  style={{ background: "green", color: "white" }}
-                  block
-                  onClick={openWhatsApp}
-                >
-                  SUPORTE AFILIADO
-                </Button>
-
-                <p className="my-2 text-center">(31) 3568-2676</p>
-              </Col>
-            </Col>
-          </Row>
-        )}
-      </Card>
-    </Loading>
-  );
+        </Modal>
+      )}
+    </>
+  )
 }
--- src/pages/api/login/index.js
+++ src/pages/api/login/index.js
@@ -1,6 +1,6 @@
-import { apiPratiqueFunciona, apiPratiquePro } from "@/services";
-import jwt from "jsonwebtoken";
-import utils from "@/utils";
+import { apiPratiqueFunciona, apiPratiquePro } from '@/services'
+import jwt from 'jsonwebtoken'
+import utils from '@/utils'
 
 export default async function handler(req, res) {
   //const email = 'adelmodesign@gmail.com' // nao afiliado
@@ -15,11 +15,11 @@ export default async function handler(req, res) {
       user_login: email,
       user_pass: utils.encrypt_md5(senha)
     }
-  });
+  })
 
   if (usuarioExist.length) {
-    user = usuarioExist[0];
-    user.token = jwt.sign({ ID: String(user.ID) }, process.env.JWT_SECRET);
+    user = usuarioExist[0]
+    user.token = jwt.sign({ ID: String(user.ID) }, process.env.JWT_SECRET)
 
     // afiliados
     if (user.user_status === 1) {
@@ -27,18 +27,116 @@ export default async function handler(req, res) {
         where: {
           email: email
         }
-      });
+      })
 
       if (afiliadoExist.length) {
-        user.isAffiliate = afiliadoExist.length ? afiliadoExist[0].idloja : 0;
+        user.isAffiliate = afiliadoExist.length ? afiliadoExist[0].idloja : 0
       }
     } else {
-      user.isAffiliate = 0;
+      user.isAffiliate = 0
       user.isAffiliate = 0
     }
 
     // Definir os emails e os valores correspondentes de cpf_sva
-    const emails = ['guilhermeam.ornelas@gmail.com', 'fernando@flima.com.br', 'gita@gmail.com']
+    const emails = [
+      'guilhermeam.ornelas@gmail.com',
+      'gita@gmail.com',
+      'joseantonioben04@gmail.com',
+      'bemcria@gmail.com',
+      'marketing@lecard.com.br',
+      'cristina.souza0887@gmail.com',
+      'anaa.roch4@gmail.com',
+      'samartins98@gmail.com',
+      'lorrainy_cruz@hotmail.com.br',
+      'andreiapboldt@gmail.com',
+      'slzache@hotmail.com',
+      'amandaduartecosta11@gmail.com',
+      'elaine.daros.baldam@gmail.com',
+      'flaviaronascimento@gmail.com',
+      'karlamartins.advocacia@gmail.com',
+      'andreotte@gmail.com',
+      'Milopesm2@gmail.com',
+      'Daiane.martine@outlook.com',
+      'Dmaycon328@gmail.com',
+      'vargasleao@hotmail.com',
+      'ketellemsantoskapiski@gmail.com',
+      'maryanesoares21@gmail.com',
+      'Thaynaratbabilon@gmail.com',
+      'daniellasilva974@gmail.com',
+      'kamilla.campores@hotmail.com',
+      'juniorvieira.jailton@gmail.com',
+      'lais04fo@gmail.com',
+      'graziellasouzaramos@gmail.com',
+      'lorenag_silva@hotmail.com',
+      'vrlessa@gmail.com',
+      'dandaracarneiro.silva19@gmail.com',
+      'jessicarfonseca98@gmail.com',
+      'anavitoria071211@gmail.com',
+      'leticiafpcorrea@gmail.com',
+      'eta-pv@hotmail.com',
+      'karlak89@gmail.com',
+      'adri10olive@gmail.com',
+      'douglas.rh@outlook.com',
+      'maria.crng19@icloud.com',
+      'kauacamposnascimento@gmail.com',
+      'rodrigorteixeira@hotmail.com',
+      'renataschaefer2019@outlook.com',
+      'angelicarodriguesmartins@hotmail.com',
+      'luisifrutofiel@gemail.com',
+      'jessykcoelho@hotmail.com',
+      'trabalhoyasmimcarvalho@gmail.com',
+      'patriciasiq1999@gmail.com.br',
+      'Daianarodriguescoutinho977@gmail.com',
+      'thalicia08@gmail.com',
+      'Thamiris.96.santana@gmail.com',
+      'gilmarlimasilva@gmail.com',
+      'Bhrenda.anna2@gmail.com',
+      'ferreiracoelho.b@gmail.com',
+      'daianadesouza018@gmail.com',
+      'esthellam61@gmail.com',
+      'fran_lopes15@hotmail.com',
+      'limajady21@gmail.com',
+      'keltonkms@hotmail.com',
+      'lorranyfraga58@gmail.com',
+      'polly.macedo22@gmail.com',
+      'pereirarizielly@gmail.com',
+      'geiradasilvarosiely@gmail.com',
+      'samara.rp@hotmail.com',
+      'sanlleybraganca@gmail.com',
+      'alana.celestino@hotmail.com',
+      'estela.as@gmail.com',
+      'IWIN.VIX@HOTMAIL.COM',
+      'J.SSICA@HOTMAIL.COM',
+      'HENRIQUE_FLV@HOTMAIL.COM',
+      'ANDRE.BARBOSA.SANCHEL@GMAIL.COM',
+      'JESSICA22SOARES@HOTMAIL.COM',
+      'andre.sarmento@gmail.com',
+      'danielerossetto4@gmail.com',
+      'leonaracastiglioni.rodrigues@gmail.com',
+      'esthefanyfoliveira@gmail.com',
+      'amanda0611silva@gmail.com',
+      'alessanderconstantino162@hotmail.com',
+      'dlsantos589632147@gmail.com',
+      'Pretty_nine@hotmail.com',
+      'wandalmazo@hotmail.com',
+      'juprado.guimaraes@gmail.com',
+      'tsegestao@gmail.com',
+      'cassiano.orsi@gmail.com',
+      'erlyvieira@gmail.com',
+      'lucascorradi.lc@gmail.com',
+      'guilherme.vituri.cordeiro@gmail.com',
+      'denildacsr@gmail.com.br',
+      'crislainecore02@gmail.com',
+      'sthefanypv6@gmail.com',
+      'borlothjacqueline@gmail.com',
+      'mellinaazevedo22@hotmail.com',
+      'joelguilherme2@gmail.com',
+      'keltonkms@hotmail.com',
+      'idalianes@hotmail.com',
+      'renzo_zerman@hotmail.com',
+      'favorellioliveira@gmail.com',
+      'paulafs10@yahoo.com.br'
+    ]
 
     // Verificar se o email do usuário está na lista
     if (emails.indexOf(user.user_email) !== -1) {
@@ -53,32 +151,32 @@ export default async function handler(req, res) {
       where: {
         email: email
       }
-    });
+    })
 
-    const professor = user.professor;
-    const curriculo = user.curriculo;
-    const cpf = user.cpf;
-    const estado = user.estado;
-    const cidade = user.cidade;
-    const telefone = user.telefone;
+    const professor = user.professor
+    const curriculo = user.curriculo
+    const cpf = user.cpf
+    const estado = user.estado
+    const cidade = user.cidade
+    const telefone = user.telefone
 
-    user.isEmployee = funcionarioExists.length ? 1 : 0;
-    user.cargo = funcionarioExists.length ? funcionarioExists[0].cargo : 0;
-    user.cpf = funcionarioExists.length ? funcionarioExists[0].cpf : 0;
-    user.idUnid = funcionarioExists.length ? funcionarioExists[0].unidade : 0;
-    user.curriculo = curriculo;
-    user.professor = professor;
-    user.cpf = cpf;
-    user.cidade = cidade;
-    user.estado = estado;
-    user.telefone = telefone;
+    user.isEmployee = funcionarioExists.length ? 1 : 0
+    user.cargo = funcionarioExists.length ? funcionarioExists[0].cargo : 0
+    user.cpf = funcionarioExists.length ? funcionarioExists[0].cpf : 0
+    user.idUnid = funcionarioExists.length ? funcionarioExists[0].unidade : 0
+    user.curriculo = curriculo
+    user.professor = professor
+    user.cpf = cpf
+    user.cidade = cidade
+    user.estado = estado
+    user.telefone = telefone
 
     // pacto
     const pactoExist = await apiPratiquePro.matriz.findMany({
       where: {
         matriz_email: email
       }
-    });
+    })
 
     if (pactoExist.length) {
       const unidadeExist = await apiPratiquePro.unidade.findMany({
@@ -88,38 +186,38 @@ export default async function handler(req, res) {
         select: {
           unidade_nome: true
         }
-      });
-      user.status = pactoExist[0].matriz_situacao;
-      user.plano = pactoExist[0].matriz_plano;
+      })
+      user.status = pactoExist[0].matriz_situacao
+      user.plano = pactoExist[0].matriz_plano
 
       if (unidadeExist.length > 0) {
-        user.unidade = unidadeExist[0].unidade_nome;
+        user.unidade = unidadeExist[0].unidade_nome
       } else {
-        user.unidade = "Pratique Fitness";
+        user.unidade = 'Pratique Fitness'
       }
 
       // Verificar se matriz_tel existe antes de atribuir a user.telefone
       if (pactoExist[0].matriz_tel !== undefined) {
-        user.telefone = pactoExist[0].matriz_tel;
+        user.telefone = pactoExist[0].matriz_tel
       } else {
-        user.telefone = null;
+        user.telefone = null
       }
 
       // Verificar se matriz_cpf existe antes de atribuir a user.cpf
       if (pactoExist[0].matriz_cpf !== undefined) {
-        user.cpf = pactoExist[0].matriz_cpf;
+        user.cpf = pactoExist[0].matriz_cpf
       } else {
-        user.cpf = null;
+        user.cpf = null
       }
     } else {
-      user.status = null;
-      user.plano = null;
-      user.unidade = null;
-      user.telefone = user.telefone ?? null;
-      user.isAffiliate;
+      user.status = null
+      user.plano = null
+      user.unidade = null
+      user.telefone = user.telefone ?? null
+      user.isAffiliate
       //user.cpf = null
     }
-    console.log("Valor de user.plano:", user.plano);
+    console.log('Valor de user.plano:', user.plano)
 
     // Verificação do termo "PERSONAL" no plano
     // if (user.plano?.includes('PERSONAL')) {
@@ -127,5 +225,5 @@ export default async function handler(req, res) {
     //}
   }
 
-  res.status(200).json(utils.clearDatabaseResult([user]));
+  res.status(200).json(utils.clearDatabaseResult([user]))
 }
